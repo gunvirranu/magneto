74899f2d19ec850c6ddf6b45c7338f8eb046d439
Gunvir Ranu <mail@gunvirranu.com>
2022-06-20 01:32:26 -0700

    Impl math and WGS84 constants

--- include/magneto/magneto.h
+++ include/magneto/magneto.h
@@ -1,6 +1,28 @@
 #ifndef MAGNETO_MAGNETO_H
 #define MAGNETO_MAGNETO_H
 
-void hello(void);
+#ifdef MAGNETO_SINGLE_PRECISION
+typedef float magneto_real;
+#define MAGNETO_REAL(x) x ## f
+#else
+typedef double magneto_real;
+#define MAGNETO_REAL(x) x
+#endif
+
+/// Pi
+extern const magneto_real magneto_pi;
+/// Equatorial radius or semi-major axis of the WGS84 ellipse in [m]
+extern const magneto_real magneto_wgs84_a;
+/// Flattening of the WGS84 ellipsoid
+extern const magneto_real magneto_wgs84_f;
+/// Reciprocal flattening (1/f) of the WGS ellipsoid
+extern const magneto_real magneto_wgs84_f_inv;
+/// Eccentricity squared (e^2) of the WGS84 ellipsoid
+extern const magneto_real magneto_wgs84_e_sq;
+
+/// Convert radians to degrees
+magneto_real magneto_rad_to_deg(magneto_real rad);
+/// Convert degrees to radians
+magneto_real magneto_deg_to_rad(magneto_real deg);
 
 #endif  // MAGNETO_MAGNETO_H
--- src/magneto.c
+++ src/magneto.c
@@ -1,7 +1,22 @@
 #include "magneto/magneto.h"
 
-#include <stdio.h>
+#define C(x) MAGNETO_REAL(x)
 
-void hello(void) {
-    printf("Hello, World!\n");
+typedef magneto_real real;
+
+const real magneto_pi = C(3.141592653589793238462643383279);
+const real magneto_rad_per_deg = C(0.017453292519943295769);
+const real magneto_deg_per_rad = C(57.29577951308232087680);
+
+const real magneto_wgs84_a = C(6378137.0);
+const real magneto_wgs84_f = (real) (1000000000.0 / 298257223563LL);
+const real magneto_wgs84_f_inv = (real) (298257223563LL / 1000000000.0);
+const real magneto_wgs84_e_sq = (magneto_wgs84_f * (2 - magneto_wgs84_f));
+
+real magneto_rad_to_deg(const real rad) {
+    return rad * magneto_deg_per_rad;
+}
+
+real magneto_deg_to_rad(const real deg) {
+    return deg * magneto_rad_per_deg;
 }
