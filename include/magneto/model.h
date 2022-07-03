0cf5271d12a058f46153188501341858f4189bc5
Gunvir Ranu <mail@gunvirranu.com>
2022-07-02 17:47:12 -0700

    Add magnetic field model type

---
+++ include/magneto/model.h
@@ -0,0 +1,31 @@
+#ifndef MAGNETO_MODEL_H
+#define MAGNETO_MODEL_H
+
+#include <stddef.h>
+
+#include "magneto.h"
+
+#define MAGNETO_CALC_INDEX(n, m) (((n) * ((n) + 1) / 2) + (m) - 1)
+
+typedef struct {
+    const magneto_real g;
+    const magneto_real h;
+} magneto_SphericalHarmonicCoeff;
+
+typedef struct {
+    // Length is `magneto_Model.num_model_coeffs`
+    const magneto_SphericalHarmonicCoeff *const coeffs;
+} magneto_ModelCoeffs;
+
+typedef struct {
+    const magneto_DecYear epoch;
+    const size_t nm_max;
+    const size_t num_model_coeffs;
+    const size_t num_models;
+    const magneto_DecYear model_interval;
+    // Length is `num_models`
+    const magneto_ModelCoeffs *const models;
+    const magneto_ModelCoeffs last_secular;
+} magneto_Model;
+
+#endif  // MAGNETO_MODEL_H
---
+++ src/model.c
@@ -0,0 +1,7 @@
+#include "magneto/model.h"
+
+#include <stddef.h>
+
+#include "common_private.h"
+
+// FIXME: Add stuff
